9a0c9aab9370b927d8f44a657293e43e4779db2d
qdzhang <qdzhangcn@gmail.com>
2021-12-18 13:13:23 +0800

    Setup borg and epkgs

--- init.el
+++ init.el
@@ -1,25 +1,51 @@
 ;;; init.el -*- lexical-binding: t; -*-
 
-;; Set path to dependencies
-(setq site-lisp-dir
-      (expand-file-name "site-lisp" user-emacs-directory))
+;;; Early birds
+(progn                                  ;     startup
+  (defvar before-user-init-time (current-time)
+    "Value of `current-time' when Emacs begins loading `user-init-file'.")
+  (message "Loading Emacs...done (%.3fs)"
+           (float-time (time-subtract before-user-init-time
+                                      before-init-time)))
+  (message "Loading %s..." user-init-file)
+  (when (< emacs-major-version 27)
+    (setq package-enable-at-startup nil)
+    (load-file (expand-file-name "early-init.el" user-emacs-directory))))
 
+(eval-and-compile ; `borg'
+  (add-to-list 'load-path (expand-file-name "lib/borg" user-emacs-directory))
+  (require 'borg)
+  (borg-initialize))
+
+
+
+;; Set path to dependencies
 (setq config-dir
       (expand-file-name "config" user-emacs-directory))
 
 ;; Set up load path
 (add-to-list 'load-path config-dir)
-(add-to-list 'load-path site-lisp-dir)
 
 (require 'cl-lib)
 (require 'init-misc)
 (require 'init-appearance)
 (require 'init-parens)
+(require 'init-git)
 
-(add-hook 'emacs-startup-hook
-          (lambda ()
-            (message "Emacs ready in %s with %d garbage collections."
-                     (format "%.2f seconds"
-                             (float-time
-                              (time-subtract after-init-time before-init-time)))
-                     gcs-done)))
+(progn ;     startup
+  (message "Loading %s...done (%.3fs)" user-init-file
+           (float-time (time-subtract (current-time)
+                                      before-user-init-time)))
+  (add-hook 'after-init-hook
+            (lambda ()
+              (message
+               "Loading %s...done (%.3fs) [after-init]" user-init-file
+               (float-time (time-subtract (current-time)
+                                          before-user-init-time))))
+            t))
+
+(progn ;     personalize
+  (let ((file (expand-file-name (concat (user-real-login-name) ".el")
+                                user-emacs-directory)))
+    (when (file-exists-p file)
+      (load file))))
