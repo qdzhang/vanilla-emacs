1ac210ca885eef2c502795396bcaa4aac6efa9df
qdzhang <qdzhangcn@gmail.com>
2022-04-29 19:57:36 +0800

    Config god-mode
    - Add god-mode and its configurations
    - Add rich-minority to hide some minor modes in modeline
    - Make god-mode modeline lighter red

---
+++ config/init-god.el
@@ -0,0 +1,60 @@
+;;; init-god.el --- Config god-mode                  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;; Config god-mode
+
+;;; Code:
+
+(setq god-mode-enable-function-key-translation nil)
+
+(require 'god-mode)
+(require 'god-mode-isearch)
+
+(defun god-update-cursor ()
+  "Update my cursor."
+  (setq cursor-type
+        (if god-local-mode
+            'box
+          'bar)))
+
+(defun god-toggle-on-overwrite ()
+  "Toggle god-mode on overwrite-mode."
+  (if (bound-and-true-p overwrite-mode)
+      (god-local-mode-pause)
+    (god-local-mode-resume)))
+
+;; Keybindings
+
+(global-set-key (kbd "C-x C-1") 'delete-other-windows)
+(global-set-key (kbd "C-x C-2") 'split-window-below)
+(global-set-key (kbd "C-x C-3") 'split-window-right)
+(global-set-key (kbd "C-x C-0") 'delete-window)
+
+(define-key god-local-mode-map (kbd ".") 'repeat)
+
+;; https://www.reddit.com/r/emacs/comments/3pk8rl/comment/cw7l5n8/?utm_source=share&utm_medium=web2x&context=3
+(defun my/god-esc ()
+  "If the minibuffer is open, ESC will close it;
+Otherwise, invoke `god-mode-all'"
+  (interactive)
+  (if (and (>= (recursion-depth) 1)
+           (active-minibuffer-window))
+      (keyboard-escape-quit)
+    (god-mode-all)))
+(global-set-key (kbd "<escape>") 'my/god-esc)
+(define-key god-local-mode-map (kbd "DEL") 'delete-backward-char)
+
+(define-key isearch-mode-map (kbd "<escape>") 'god-mode-isearch-activate)
+(define-key god-mode-isearch-map (kbd "<escape>") 'god-mode-isearch-disable)
+
+;; Hooks
+
+(add-hook 'god-mode-enabled-hook 'god-update-cursor)
+(add-hook 'god-mode-disabled-hook 'god-update-cursor)
+(add-hook 'overwrite-mode-hook 'god-toggle-on-overwrite)
+
+(god-mode)
+
+(provide 'init-god)
+;;; init-god.el ends here
---
+++ config/init-modeline.el
@@ -0,0 +1,26 @@
+;;; init-modeline.el --- Config modeline             -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;; Config modeline and diminish some minor modes
+
+;;; Code:
+
+(require 'rich-minority)
+(rich-minority-mode 1)
+
+(setq useless-minor-modes
+      '(" company"
+        "hl-p"
+        "super-save"
+        "ElDoc"
+        "SP"
+        "ws"
+        "=>"))
+(setq rm-blacklist (mapconcat 'identity useless-minor-modes "\\| "))
+
+(with-eval-after-load 'rich-minority
+  (add-to-list 'rm-text-properties '("\\` God\\'" 'face '(:foreground "red"))))
+
+(provide 'init-modeline)
+;;; init-modeline.el ends here
