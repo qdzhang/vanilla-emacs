7103df4e9f6517671cce103b9393d15fb49dc81f
qdzhang <qdzhangcn@gmail.com>
2022-05-14 21:24:06 +0800

    Config eshell prompt

--- config/init-eshell.el
+++ config/init-eshell.el
@@ -20,4 +20,53 @@
 (defalias 'e 'eshell)
 (defalias 'at 'ansi-term)
 
+
+(defun with-face (str &rest face-plist)
+  (propertize str 'face face-plist))
+
+(defun fish-path (path max-len)
+  "Return a potentially trimmed-down version of the directory PATH, replacing
+parent directories with their initial characters to try to get the character
+length of PATH (sans directory slashes) down to MAX-LEN."
+  (let* ((components (split-string (abbreviate-file-name path) "/"))
+         (len (+ (1- (length components))
+                 (cl-reduce '+ components :key 'length)))
+         (str ""))
+    (while (and (> len max-len)
+                (cdr components))
+      (setq str (concat str
+                        (cond ((= 0 (length (car components))) "/")
+                              ((= 1 (length (car components)))
+                               (concat (car components) "/"))
+                              (t
+                               (if (string= "."
+                                            (string (elt (car components) 0)))
+                                   (concat (substring (car components) 0 2)
+                                           "/")
+                                 (string (elt (car components) 0) ?/)))))
+            len (- len (1- (length (car components))))
+            components (cdr components)))
+    (concat str (cl-reduce (lambda (a b) (concat a "/" b)) components))))
+
+(defun shk-eshell-prompt ()
+  (let ((blue "DodgerBlue"))
+    (concat
+     (with-face (concat (fish-path (eshell/pwd) 40) " ")
+                :foreground blue)
+     (with-face
+      (or (ignore-errors (format "(%s)" (vc-responsible-backend default-directory))) ""))
+     (when (featurep 'chruby)
+       (with-face (concat " <"
+                          (chruby-current)
+                          ">")
+                  :foreground "SpringGreen"))
+     (with-face "\n" :foreground blue)
+     (if (= (user-uid) 0)
+         (with-face "#" :foreground "red")
+       "λ")
+     " ")))
+(setq eshell-prompt-function 'shk-eshell-prompt)
+(setq eshell-highlight-prompt nil)
+
+
 (provide 'init-eshell)
