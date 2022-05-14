eb2e6d724539790e6e07cef1233ec755093dd851
qdzhang <qdzhangcn@gmail.com>
2022-05-14 20:19:00 +0800

    Add package: chruby

--- config/init-ruby.el
+++ config/init-ruby.el
@@ -11,5 +11,11 @@
 (autoload 'inf-ruby-minor-mode "inf-ruby" "Run an inferior Ruby process" t)
 (add-hook 'ruby-mode-hook 'inf-ruby-minor-mode)
 
+(setq ruby-deep-indent-paren nil)
+
+
+(require 'chruby)
+(add-hook 'ruby-mode-hook #'chruby-use-corresponding)
+
 (provide 'init-ruby)
 ;;; init-ruby.el ends here
