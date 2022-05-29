08d6eb55aa620a0cb9c9436f050e5faa71e6226e
qdzhang <qdzhangcn@gmail.com>
2022-05-29 21:03:49 +0800

    Diable eglot events buffer

--- config/init-eglot.el
+++ config/init-eglot.el
@@ -8,6 +8,9 @@
 
 (require 'eglot)
 
+;; Disable eglot events buffer.
+(setq eglot-events-buffer-size 0)
+
 (add-to-list 'eglot-server-programs '((c++-mode c-mode) "ccls"))
 (add-to-list 'eglot-server-programs '(python-mode . ("pyright-langserver" "--stdio")))
 (add-to-list 'eglot-server-programs '(d-mode . ("/usr/bin/serve-d")))
