e3b1b669ae3838588d30630d1aa76cad7c0e5dce
qdzhang <qdzhangcn@gmail.com>
2022-07-19 15:03:39 +0800

    Remove rich-minority package

--- config/init-modeline.el
+++ config/init-modeline.el
@@ -11,20 +11,5 @@
  `(mode-line ((t (:inherit variable-pitch :height 1.0 :box (:line-width 2 :color "#000000") :background "#c1c1c1" :foreground "#000000"))))
  `(mode-line-inactive ((t (:inherit variable-pitch :height 1.0 :box (:line-width 2 :color "#676767") :background "#eeeeee" :foreground "#000000")))))
 
-(require 'rich-minority)
-(rich-minority-mode 1)
-
-(setq useless-minor-modes
-      '(" hl-p"
-        "super-save"
-        "ElDoc"
-        "SP"
-        "ws"
-        "=>"))
-(setq rm-blacklist (mapconcat 'identity useless-minor-modes "\\| "))
-
-(with-eval-after-load 'rich-minority
-  (add-to-list 'rm-text-properties '("\\` God\\'" 'face '(:foreground "red"))))
-
 (provide 'init-modeline)
 ;;; init-modeline.el ends here
