8f74c465195d7dcac0831a7468bb1a443a867feb
qdzhang <qdzhangcn@gmail.com>
2022-10-31 20:54:51 +0800

    Update python-mode hooks

--- config/init-python.el
+++ config/init-python.el
@@ -8,7 +8,21 @@
 
 ;;; Code:
 
-(add-hook 'python-mode-hook 'smart-dash-mode)
+(add-hook 'python-mode-hook
+          (lambda ()
+            (smart-dash-mode 1)
+            (hs-minor-mode 1)
+            (setq imenu-create-index-function 'python-imenu-create-flat-index)
+            (imenu-add-menubar-index)
+            (setq-local compile-command
+                        (concat "pytest -v "
+                                (when buffer-file-name
+                                  (shell-quote-argument buffer-file-name)))
+                        compilation-scroll-output t)))
+(add-hook 'inferior-python-mode-hook
+          (lambda ()
+            (smart-dash-mode 1)))
+
 
 (provide 'init-python)
 ;;; init-python.el
