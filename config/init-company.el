9ca9977360b41deb47615745a506575f58584d86
qdzhang <qdzhangcn@gmail.com>
2022-11-23 15:51:18 +0800

    Add posframe and use company-posframe
    Assimilate 2 drones
    
    - Assimilate company-posframe v0.5.0-21-gab58972
    - Assimilate posframe         v1.2.0

--- config/init-company.el
+++ config/init-company.el
@@ -8,6 +8,11 @@
 (setq completion-ignore-case t)
 (setq company-dabbrev-downcase nil)
 
+(require 'company-posframe)
+(company-posframe-mode 1)
+;; Disable the auto quickhelp frame. Use <f1> to open quickhelp manually
+(setq company-posframe-quickhelp-delay nil)
+
 (with-eval-after-load 'company-mode
   (define-key company-active-map (kbd "RET") 'company-complete-selection)
   (define-key company-active-map [return] 'company-complete-selection))
