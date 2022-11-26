e9e44aba69f865589d378858b5b309611ae665bd
qdzhang <qdzhangcn@gmail.com>
2022-11-26 19:41:38 +0800

    Create missing directory using a function hook
    Formerly I am using advices to achieve this. bbatsov introduce a
    neat approach in emacsredux's blog.

--- config/init-misc.el
+++ config/init-misc.el
@@ -27,11 +27,17 @@
 ;;; When use FIND-FILE to create a new file, if the directory doesn't exist yet,
 ;;; create the directory
 ;;; https://superuser.com/questions/131538/can-i-create-directories-that-dont-exist-while-creating-a-new-file-in-emacs
-(defadvice find-file (before make-directory-maybe (filename &optional wildcards) activate)
-  "Create parent directory if not exists while visiting file."
-  (unless (file-exists-p filename)
-    (let ((dir (file-name-directory filename)))
-      (unless (file-exists-p dir)
-        (make-directory dir t)))))
+;; Formerly I am using advices to achieve this like the answer in previous link.
+;; But adding a procedure for `find-file-not-found-functions' hook is simpler.
+(defun my/auto-create-missing-dirs ()
+  "Create the directory if new file is created in a nonexist directory.
+
+URL: https://emacsredux.com/blog/2022/06/12/auto-create-missing-directories/"
+  (let ((target-dir (file-name-directory buffer-file-name)))
+    (unless (file-exists-p target-dir)
+      (make-directory target-dir t))))
+
+(add-to-list 'find-file-not-found-functions #'my/auto-create-missing-dirs)
+
 
 (provide 'init-misc)
