2ba5925be3b6ee9f9c1dc5b3b3e1c1031bf01e7d
qdzhang <qdzhangcn@gmail.com>
2022-11-27 19:07:18 +0800

    Tidy lazy-key configuration

--- config/init-lazy-keys.el
+++ config/init-lazy-keys.el
@@ -1,45 +1,14 @@
 ;;; init-lazy-keys.el --- Init for lazy load keys  -*- lexical-binding: t; -*-
 
-;;; newline-without-break-of-line
-;;;###autoload
-(defun my/newline-without-break-of-line ()
-  "1. move to end of the line.
-  2. insert newline with index"
-
-  (interactive)
-  (let ((oldpos (point)))
-    (end-of-line)
-    (newline-and-indent)))
-
-;; (global-set-key (kbd "<C-return>") 'my/newline-without-break-of-line)
-;; (lazy-load-global-keys
-;;  '(
-;;    ("C-<return>" . my/newline-without-break-of-line)))
-
 ;; Make ESC quit prompts
 ;; (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
 ;; Use `comment-line' to replace `comment-dwim'
 (global-set-key [remap comment-dwim] #'comment-line)
 
-
-;;; === Git config ===
-
 (lazy-load-global-keys
  '(("C-c g" . my-transient/magit-menu))
  "init-git")
-
-;;; === Git config ===
-
-;; (lazy-load-unset-keys
-;;  '("M-J" "M-r" "M-s" "M-;" "C-M-f" "C-M-b" "M-)")
-;;  smartparens-mode-map)             ;卸载按键
-;; (defvar smartparens-key-alist nil)
-;; (setq smartparens-key-alist
-;;       '(
-
-;;         ))
-;; (lazy-load-set-keys smartparens-key-alist smartparens-mode-map)
 
 (lazy-load-global-keys
  '(("C-M-s" . my-transient/smartparens-menu))
@@ -70,7 +39,6 @@
    ("M-d" . my/sp-kill-word)
    ("C-w" . my/kill-region-or-line)
    ("C-;" . sp-comment))
-
  smartparens-mode-map
  "init-smartparens")
 
