ce83b59f4bf3c34a8e03ea76e21b40103285e088
qdzhang <qdzhangcn@gmail.com>
2022-12-05 16:17:46 +0800

    Remove hidden-mode-line-mode

--- config/init-modeline.el
+++ config/init-modeline.el
@@ -15,33 +15,5 @@
                   (_ elem)))
               mode-line-modes))
 
-;; Hide mode line
-;; Ref: https://bzg.fr/en/emacs-hide-mode-line/
-(defvar-local hidden-mode-line-mode nil)
-
-(define-minor-mode hidden-mode-line-mode
-  "Minor mode to hide the mode-line in the current buffer."
-  :init-value nil
-  :global t
-  :variable hidden-mode-line-mode
-  :group 'editing-basics
-  (if hidden-mode-line-mode
-      (setq hide-mode-line mode-line-format
-            mode-line-format nil)
-    (setq mode-line-format hide-mode-line
-          hide-mode-line nil))
-  (force-mode-line-update)
-  ;; Apparently force-mode-line-update is not always enough to
-  ;; redisplay the mode-line
-  (redraw-display)
-  (when (and (called-interactively-p 'interactive)
-             hidden-mode-line-mode)
-    (run-with-idle-timer
-     0 nil 'message
-     (concat "Hidden Mode Line Mode enabled.  "
-             "Use M-x hidden-mode-line-mode to make the mode-line appear."))))
-
-(global-set-key (kbd "<f9>") 'hidden-mode-line-mode)
-
 (provide 'init-modeline)
 ;;; init-modeline.el ends here
