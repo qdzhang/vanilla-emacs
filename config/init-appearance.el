7f069bbc1eeb3840d2cad1f4bd3722e168962b41
qdzhang <qdzhangcn@gmail.com>
2023-01-13 20:17:23 +0800

    Update custom theme

--- config/init-appearance.el
+++ config/init-appearance.el
@@ -28,7 +28,8 @@
 
 
 ;; Sarasa font set for CJK font
-(set-fontset-font t 'han "Sarasa Mono SC")
+;; (set-fontset-font t 'han "Sarasa Mono SC")
+(set-fontset-font t 'han "HYXuanSong")
 (set-fontset-font t 'kana "Sarasa Mono J")
 (set-fontset-font t 'hangul "Sarasa Mono K")
 (set-fontset-font t 'cjk-misc "Sarasa Mono SC")
