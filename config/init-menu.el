54497895bc4049b8733254024f0199a8dbe8678c
qdzhang <qdzhangcn@gmail.com>
2023-02-02 19:51:02 +0800

    Add sdcv to context-menu and config the submenu

--- config/init-menu.el
+++ config/init-menu.el
@@ -8,6 +8,8 @@
 
 (context-menu-mode 1)
 
+(add-hook 'context-menu-functions #'context-menu-ffap)
+
 (defun highlight-symbol-at-mouse (e)
   "Highlight symbol at mouse click E."
   (interactive "e")
@@ -21,12 +23,30 @@
     (mouse-set-point click)
     (when (symbol-at-point)
       (define-key-after menu [highlight-search-separator] menu-bar-separator)
-      (define-key-after menu [highlight-search-mouse]
-        '(menu-item "Highlight Symbol" highlight-symbol-at-mouse
-                    :help "Highlight symbol at point"))))
+      (let ((submenu (make-sparse-keymap (propertize "Highlight"))))
+        (define-key-after submenu [highlight-search-mouse]
+          `(menu-item "Highlight Symbol" highlight-symbol-at-mouse
+                      :help "Highlight symbol at point"))
+        (define-key-after submenu [unhighlight-symbol]
+          `(menu-item "Unhighlight Regexp" unhighlight-regexp
+                      :help "Unhighlight selected regexp"))
+        (define-key-after menu [highlight-search-mouse]
+          `(menu-item "Highlight" ,submenu)))))
   menu)
 
 (add-hook 'context-menu-functions #'context-menu-highlight-symbol)
+
+(defun context-menu-inf-sdcv (menu click)
+  "Menu entry to search current word using inf-sdcv"
+  (define-key-after menu [inf-sdcv]
+    '(menu-item "Inf-sdcv" (lambda (click)
+                             (interactive "e")
+                             (require 'inf-sdcv-mode)
+                             (inf-sdcv-search-at-point))
+                :help "Search current word using inf-sdcv"))
+  menu)
+
+(add-hook 'context-menu-functions #'context-menu-inf-sdcv)
 
 (provide 'init-menu)
 ;;; init-menu.el ends here
