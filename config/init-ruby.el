e22ceece623cf11966cda9f0537fc2081aa0ed80
qdzhang <qdzhangcn@gmail.com>
2023-08-24 20:30:57 +0800

    Add rspec-mode and some ruby-relative config

--- config/init-ruby.el
+++ config/init-ruby.el
@@ -81,6 +81,41 @@
 
 (add-hook 'ruby-mode-local-vars-hook 'my/ruby-maybe-highlight-debugger-keywords)
 
+(defun my/ruby-quick-run-in-ansi-term ()
+  "Quick run current ruby file in `ansi-term'."
+  (interactive)
+  (if (and (eq major-mode 'ruby-mode)
+           (file-exists-p (buffer-name)))
+      (comint-send-string "*ansi-term*"
+                          (concat "ruby "
+                                  (buffer-name)
+                                  "\n"))
+    (message "The current buffer isn't associated with a file!")))
+
+(defun my/inf-ruby-auto-enter ()
+  "Automatically enters inf-ruby-mode in ruby modes' debugger breakpoints.
+Add this function to the hook of `compilation-mode'."
+  (add-hook 'compilation-filter-hook 'inf-ruby-auto-enter nil t))
+
+(with-eval-after-load 'rspec-mode
+  (add-hook 'rspec-compilation-mode-hook 'my/inf-ruby-auto-enter))
+
+;; Use breakpoints binding.pry or byebug
+;; When you've hit the breakpoint, hit C-x C-q to enable inf-ruby.
+(add-hook 'ruby-mode-hook 'inf-ruby-switch-setup)
+
+;; Setup built-in `compilation'
+(setq compilation-scroll-output 'first-error)
+
+(defun my/compilation-colorize ()
+  "Colorize from `compilation-filter-start' to `point'."
+  (require 'ansi-color)
+  (let ((inhibit-read-only t))
+    (ansi-color-apply-on-region (point-min) (point-max))))
+
+(add-hook 'compilation-filter-hook #'my/compilation-colorize)
+
+
 
 (provide 'init-ruby)
 ;;; init-ruby.el ends here
