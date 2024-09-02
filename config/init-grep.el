699f6051e3314de22c2f55b78f6f6e94f1d2c1a2
qdzhang <qdzhangcn@gmail.com>
2024-09-02 21:19:24 +0800

    Run rg-define-search after rg-menu

--- config/init-grep.el
+++ config/init-grep.el
@@ -21,14 +21,14 @@
 (require 'rg)
 (require 'rg-isearch)
 
-;;;###autoload (autoload 'rg-ask-current-file "rg.el" "" t)
-(rg-define-search rg-ask-current-file
-  "Search for thing in files matching the current file
+(with-eval-after-load 'rg-menu
+  (rg-define-search rg-ask-current-file
+    "Search for thing in files matching the current file
 name (as a pattern) under the current directory."
-  :query ask
-  :format literal
-  :files (rg-get-buffer-file-name)
-  :dir current
-  :menu ("Search" "g" "Grep current file"))
+    :query ask
+    :format literal
+    :files (rg-get-buffer-file-name)
+    :dir current
+    :menu ("Search" "g" "Grep current file")))
 
 (provide 'init-grep)
