cd23a47999f4de93dbe85b0b1c877953f4703954
qdzhang <qdzhangcn@gmail.com>
2025-01-02 21:10:46 +0800

    Update super-save to v0.4.0-8-gb612da0

--- config/init-super-save.el
+++ config/init-super-save.el
@@ -4,12 +4,22 @@
 (setq auto-save-default nil)
 
 (require 'super-save)
-(super-save-mode +1)
 
+
+(add-to-list 'super-save-triggers 'my/quick-window-jump)
 (add-to-list 'super-save-hook-triggers 'find-file-hook)
 (add-to-list 'super-save-triggers 'switch-to-buffer)
 (add-to-list 'super-save-triggers 'tab-bar-switch-to-tab)
+
+(super-save-mode +1)
+
 (setq super-save-auto-save-when-idle t)
 (setq super-save-exclude '(".gpg"))
+;; Enable deleting trailing white spaces before saving (except for the current line)
+(setq super-save-delete-trailing-whitespace 'except-current-line)
+;; Save silently
+(setq super-save-silent t)
+;; Save all buffer
+(setq super-save-all-buffers t)
 
 (provide 'init-super-save)
