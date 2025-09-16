0fa5409516507a36d1dbb9ff22324f8eeef6c534
qdzhang <qdzhangcn@gmail.com>
2025-09-16 20:15:31 +0800

    Remove clang-rename

--- config/init-clang-tools.el
+++ config/init-clang-tools.el
@@ -5,11 +5,14 @@
 ;; `clang-format' and `clang-rename' is handy to use.
 ;; Load system built-in `clang-format.el' and `clang-rename.el'
 ;; when the function is invocated
+;;
+;; Notice:
+;; Since Clang 20.1.0, the `clang-rename' tool has been removed.
+;; Ref: https://releases.llvm.org/20.1.0/tools/clang/docs/ReleaseNotes.html#potentially-breaking-changes
 
 ;;; Code:
 
 (load "/usr/share/clang/clang-format.el")
-(load "/usr/share/clang/clang-rename.el")
 
 (setq-default clang-format-style "file")
 (setq-default clang-format-fallback-style "llvm")
