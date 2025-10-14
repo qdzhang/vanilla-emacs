67827c2bff7985d0b53c013c4fb106a46427e7ef
qdzhang <qdzhangcn@gmail.com>
2025-10-14 08:52:37 +0800

    Add init-secrets

---
+++ config/init-secrets.el
@@ -0,0 +1,14 @@
+;;; init-secrets.el --- Secrets                      -*- lexical-binding: t; -*-
+
+;; This file is not part of GNU Emacs.
+
+;;; Commentary:
+
+;; Put your secrets here
+
+;;; Code:
+
+
+
+(provide 'init-secrets)
+;;; init-secrets.el
