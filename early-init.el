248ad981ee3f651fe644423bf1d664199452aa9d
qdzhang <qdzhangcn@gmail.com>
2025-10-16 19:09:20 +0800

    Add some startup optimizations from Doom Emacs

--- early-init.el
+++ early-init.el
@@ -56,6 +56,58 @@
 ;; some of the language server responses are in 800k - 3M range.
 (setq read-process-output-max (expt 2 24)) ;; 16mb
 
+;; Some startup optimizations from Doom Emacs
+;; https://github.com/doomemacs/doomemacs/blob/ef9e8d892556595fcdfd723d145c29593a67c50d/lisp/doom.el#L407
+(unless (daemonp)
+  ;; PERF: `file-name-handler-alist' is consulted on each call to `require',
+  ;;   `load', or various file/io functions (like `expand-file-name' or
+  ;;   `file-remote-p'). You get a noteable boost to startup time by unsetting
+  ;;   or simplifying its value.
+  (let ((old-value (default-toplevel-value 'file-name-handler-alist)))
+    (set-default-toplevel-value
+     'file-name-handler-alist
+     ;; HACK: The elisp libraries bundled with Emacs are either compressed or
+     ;;   not, never both. So if calc-loaddefs.el.gz exists, calc-loaddefs.el
+     ;;   won't, and vice versa. This heuristic is used to guess the state of
+     ;;   all other built-in (or site); if they're compressed, we must leave the
+     ;;   gzip file handler in `file-name-handler-alist' so Emacs knows how to
+     ;;   load them. Otherwise, we can omit it (at least during startup) for a
+     ;;   boost in package load time.
+     (if (eval-when-compile
+           (locate-file-internal "calc-loaddefs.el" load-path))
+         nil
+       (list (rassq 'jka-compr-handler old-value))))
+    ;; Remember it so it can be reset where needed.
+    (put 'file-name-handler-alist 'initial-value old-value)
+    ;; COMPAT: Eventually, Emacs will process any files passed to it via the
+    ;;   command line, and will do so *really* early in the startup process.
+    ;;   These might contain special file paths like TRAMP paths, so restore
+    ;;   `file-name-handler-alist' just for this portion of startup.
+    (define-advice command-line-1 (:around (fn args-left) respect-file-handlers)
+      (let ((file-name-handler-alist
+             (if args-left old-value file-name-handler-alist)))
+        (funcall fn args-left)))
+    ;; COMPAT: ...but restore `file-name-handler-alist' later, because it is
+    ;;   needed for handling encrypted or compressed files, among other things.
+    (add-hook 'emacs-startup-hook
+              (lambda ()
+                (set-default-toplevel-value
+                 'file-name-handler-alist
+                 ;; Merge instead of overwrite because there may have been
+                 ;; changes to `file-name-handler-alist' since startup we want
+                 ;; to preserve.
+                 (delete-dups (append file-name-handler-alist old-value))))))
+
+  (unless noninteractive
+    (setq inhibit-startup-echo-area-message user-login-name)
+    ;; PERF,UX: Prevent "For information about GNU Emacs..." line in *Messages*.
+    (advice-add #'display-startup-echo-area-message :override #'ignore)
+    ;; PERF: Suppress the vanilla startup screen completely. We've disabled it
+    ;;   with `inhibit-startup-screen', but it would still initialize anyway.
+    ;;   This involves file IO and/or bitmap work (depending on the frame type)
+    ;;   that we can no-op for a free 50-100ms saving in startup time.
+    (advice-add #'display-startup-screen :override #'ignore)))
+
 
 ;; Local Variables:
 ;; no-byte-compile: t
